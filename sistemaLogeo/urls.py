67447a8cb3ddf3c5d99ee032e8bb717246a89827
joeCuadros <jcuadrosam@unsa.edu.pe>
2024-06-20 19:44:27 -0500

    Se agrego cambiar contraseña

--- sistemaLogeo/urls.py
+++ sistemaLogeo/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', index, name="index"),
     path('login/', login_views, name='login'),
     path('logout/', logout_views, name='logout'),
+    path('cambiar_password/', cambiar_password, name='cambiar_password'),
 ]
--- sistemaLogeo/views.py
+++ sistemaLogeo/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required, user_passes_test
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import render, redirect
 from .forms import *
 
 # Pagina de inicio 
+@login_required
 def index(request):
     return render(request, 'index.html')
 
@@ -31,3 +33,16 @@ def logout_views(request):
     logout(request)
     return redirect('login')
 
+#Cambiar contraseñas
+@login_required
+def cambiar_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user) 
+            return redirect('index')
+    else:
+        form = PasswordChangeForm(request.user)
+
+    return render(request, 'cambiar_password.html', {'form': form})
